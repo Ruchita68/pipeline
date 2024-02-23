c4dc690a52662c289af1afd3d479ec8abc82cac2
Ruchita68 <150236808+Ruchita68@users.noreply.github.com>
2024-02-23 13:43:48 +0530

    Update Demo.java

--- Demo.java
+++ Demo.java
@@ -1,5 +1,7 @@
 class Demo{
-	public static void main(String args[]){
-		system.out.print("Hello java");
+	public static void main(String [] args){
+		System.out.println("Hello All");
+		System.out.println("Hello Jenkins");
+		System.out.println("Hello Git");
 	}
 }
